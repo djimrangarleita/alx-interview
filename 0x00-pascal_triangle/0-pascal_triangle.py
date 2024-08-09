42a261004a1de551641ab403d623efe063d296c8
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-10 02:31:17 +0300

    Initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+    if (n <= 0):
+        return []
+    result = [[1]]
+    for i in range(n - 1):
+        tmp = [0] + result[i] + [0]
+        row = []
+        for j in range(len(tmp) - 1):
+            row.append(tmp[j] + tmp[j + 1])
+        result.append(row)
+    return result
