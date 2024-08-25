40734099a5e1f3a3117cc3d8f5a77118cb0f1834
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-25 06:20:44 +0300

    Fix codestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,8 +1,9 @@
-"""Utf8 string validation from integer list"""
 #!/usr/bin/python3
+"""Utf8 string validation from integer list"""
 
 
 def validUTF8(data):
+    """Validate utf8 char"""
     for char in data:
         if char < 0 or char > 127:
             return False
