220fc774388457298ce6ea0d60b9ef5e5cb85a06
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-11 05:42:48 +0300

    chore: Parse logs and print result

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
         return []
     result = [[1]]
     for i in range(n - 1):
-        tmp = [0] + result[i] + [0]
+        tmp = [0] + result[-1] + [0]
         row = []
         for j in range(len(tmp) - 1):
             row.append(tmp[j] + tmp[j + 1])
---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Read from stdin and parse the logs"""
+import sys
+
+
+files_size = 0
+status = {}
+line_count = 0
+
+
+def update_stats(file_size: str, lstatus: str):
+    """Update the statistics"""
+    global files_size
+    try:
+        file_size = int(file_size)
+        files_size += file_size
+    except ValueError:
+        pass
+    try:
+        lstatus = int(lstatus)
+        status[lstatus] = status.get(lstatus, 0) + 1
+    except ValueError:
+        pass
+
+
+def print_status(status: dict):
+    """Print count of status"""
+    sorted_keys = sorted(status.keys())
+    for key in sorted_keys:
+        print("{}: {}".format(key, status.get(key)))
+
+
+try:
+    for line in sys.stdin:
+        line_count += 1
+        splitted_line = line.strip().split()
+        if len(splitted_line) == 9:
+            update_stats(splitted_line[-1], splitted_line[-2])
+        if line_count == 10:
+            print("File size: {}".format(files_size))
+            print_status(status)
+            line_count = 0
+except KeyboardInterrupt:
+    print("File size: {}".format(files_size))
+    print_status(status)
+    raise
