1fa5c8965abc0ec7024700d681114b9db6c6bb1c
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-20 00:57:21 +0300

    chore(#0): Solve coin change problem with dp tabulation

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to solve the coin change problem
+"""
+
+
+def makeChange(coins, total):
+    """Return the minimum num of coins used to make total"""
+    if total <= 0:
+        return 0
+    result = [total + 1] * (total + 1)
+    result[0] = 0
+    coins.sort(reverse=True)
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                continue
+            result[i] = min(result[i], 1 + result[i - coin])
+            if coin == i:
+                break
+    if result[total] > total:
+        return -1
+    return result[total]
