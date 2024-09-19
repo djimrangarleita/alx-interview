5bae798bd3f3a133f90812963511b309e9496c4c
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-20 01:23:02 +0300

    fix(#0): Adjust control flow in loop and init result with inf

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,15 +7,14 @@ def makeChange(coins, total):
     """Return the minimum num of coins used to make total"""
     if total <= 0:
         return 0
-    result = [total + 1] * (total + 1)
+    
+    result = [float('inf')] * (total + 1)
     result[0] = 0
+    
     for i in range(total + 1):
         for coin in coins:
-            if coin > i:
-                continue
             result[i] = min(result[i], 1 + result[i - coin])
-            if coin == i:
-                break
+    
     if result[total] > total:
         return -1
     return result[total]
