9a8b87539c7f12bbf4f7558b5d55db7acedd446a
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-20 01:29:44 +0300

    fix(#0): Code style and exit paths

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,14 +7,11 @@ def makeChange(coins, total):
     """Return the minimum num of coins used to make total"""
     if total <= 0:
         return 0
-    
     result = [float('inf')] * (total + 1)
     result[0] = 0
-    
     for i in range(total + 1):
         for coin in coins:
             result[i] = min(result[i], 1 + result[i - coin])
-    
-    if result[total] > total:
-        return -1
-    return result[total]
+    if result[total] != float('inf'):
+        return result[total]
+    return -1
