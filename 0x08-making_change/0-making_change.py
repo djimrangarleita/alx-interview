16ba8d17bc8f78b4ea06ad8155dcf3289f79b53d
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-20 01:38:55 +0300

    fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,11 +7,13 @@ def makeChange(coins, total):
     """Return the minimum num of coins used to make total"""
     if total <= 0:
         return 0
+    if not coins:
+        return -1
     result = [float('inf')] * (total + 1)
     result[0] = 0
-    for i in range(total + 1):
-        for coin in coins:
-            result[i] = min(result[i], 1 + result[i - coin])
+    for coin in coins:
+        for i in range(coin, total + 1):
+            result[i] = min(result[i], result[i - coin] + 1)
     if result[total] != float('inf'):
         return result[total]
     return -1
