5de780730db695328203f34fb801037c917cf94d
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-27 02:37:43 +0300

    fix(#0): Support cases where island border is not water

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,10 +10,13 @@ def island_perimeter(grid):
         return 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                cell_perimeter = abs(grid[i - 1][j] - 1)\
-                        + abs(grid[i][j - 1] - 1)\
-                        + abs(grid[i + 1][j] - 1)\
-                        + abs(grid[i][j + 1] - 1)
-                perimeter += cell_perimeter
+            if grid[i][j] != 0:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if j - 1 < 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i + 1 >= len(grid) or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j + 1 >= len(grid[i]) or grid[i][j + 1] == 0:
+                    perimeter += 1
     return perimeter
